3dfc943b05b972ccb55a91d543dbf90b92cebbb7
Richa Ferry Setyawan <aakricha2@gmail.com>
2026-08-12 09:29:34 +0700

    Skip Plugin Check pass when project has no .phpcs-plugin-review.xml

--- scripts/lint-phpcs.sh
+++ scripts/lint-phpcs.sh
@@ -13,13 +13,11 @@ pass2_status=0
 phpcs --standard=.phpcs.xml "$@" || pass1_status=$?
 
 # Pass 2: WordPress.org org-review (Plugin Check plugin_review_phpcs).
+# Only run when the project provides a project-level ruleset. Pro/non-wp.org
+# plugins should not be forced through this pass just because the vendor
+# ruleset happens to be installed.
 if [[ -f .phpcs-plugin-review.xml ]]; then
     phpcs --standard=.phpcs-plugin-review.xml "$@" || pass2_status=$?
-elif [[ -f vendor/publishpress/publishpress-phpcs-standards/standards/plugin-check-rulesets/plugin-review.xml ]]; then
-    phpcs --standard=vendor/publishpress/publishpress-phpcs-standards/standards/plugin-check-rulesets/plugin-review.xml "$@" || pass2_status=$?
-else
-    echo "Plugin Check ruleset not found. Run composer config:plugin-check." >&2
-    exit 1
 fi
 
 if [[ $pass1_status -ne 0 || $pass2_status -ne 0 ]]; then
